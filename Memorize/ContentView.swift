4fcabbeaf371130f70dfc59d93fde62e41ed5707
yasser tamimi <altamimiy89@gmail.com>
2021-11-25 07:01:32 +0300

    lecture 3 & 4

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,74 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
-    var emojis = [
-        "🚗", "🚕", "🚙", "🚌", "🚎", "🏎", "🚓", "🚑", "🚒", "🚐", "🛻", "🚚",
-        "🚛", "🚜", "🚍", "🚘", "🚖", "🚇", "🚊", "🛵", "🏍", "🛺", "🚨", "🚔"
-    ]
-    @State var emojiCount = 15
+    @ObservedObject var viewModel: EmojiMemoryGame
 
     var body: some View {
-        VStack {
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
-                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
-                        CardView(content: emoji)
-                            .aspectRatio(2/3, contentMode: .fit)
-                    }
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                ForEach(viewModel.cards) { card in
+                    CardView(card: card)
+                        .aspectRatio(2 / 3, contentMode: .fit)
+                        .onTapGesture {
+                            viewModel.choose(card)
+                        }
                 }
             }
-                .foregroundColor(.red)
-            Spacer()
-            HStack {
-                removeButton
-                Spacer()
-                addButton
-            }
-                .font(.largeTitle)
-                .padding(.horizontal)
         }
+            .foregroundColor(.red)
             .padding(.horizontal)
     }
 
-    var removeButton: some View {
-        Button {
-            if emojiCount > 4 {
-                emojiCount -= 1
-            }
-        } label: {
-            Image(systemName: "minus.circle")
-        }
-    }
-
-    var addButton: some View {
-        Button {
-            if emojiCount < emojis.count {
-                emojiCount += 1
-            }
-        } label: {
-            Image(systemName: "plus.circle")
-        }
-    }
 }
 
 struct CardView: View {
-    let content: String
-    @State var isFaceUp = true
+    let card: MemoryGame<String>.Card
 
     var body: some View {
         ZStack {
             let shape = RoundedRectangle(cornerRadius: 20)
 
-            if isFaceUp {
+            if card.isFaceUp {
                 shape.foregroundColor(.white)
                 shape.strokeBorder(lineWidth: 3)
-                Text(content).font(.largeTitle)
+                Text(card.content).font(.largeTitle)
+            } else if card.isMatched {
+                shape.opacity(0)
             } else {
                 shape
             }
-        }
-            .onTapGesture {
-            isFaceUp.toggle()
         }
     }
 }
@@ -84,10 +52,11 @@ struct CardView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        let game = EmojiMemoryGame()
         Group {
-            ContentView()
+            ContentView(viewModel: game)
                 .preferredColorScheme(.dark)
-            ContentView()
+            ContentView(viewModel: game)
         }
     }
 }
---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,60 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Yasser Tamimi on 25/11/2021.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> where CardContent: Comparable {
+    private(set) var cards: [Card]
+
+    private var indexOfTheOnlyFaceUpCard: Int?
+
+    init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
+        cards = [Card]()
+
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = createCardContent(pairIndex)
+            cards.append(Card(id: pairIndex * 2, content: content))
+            cards.append(Card(id: pairIndex * 2 + 1, content: content))
+        }
+    }
+
+    mutating func choose(_ card: Card) {
+        // get the card and check it's not faceUp or matched already
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }),
+            !cards[chosenIndex].isFaceUp,
+            !cards[chosenIndex].isMatched
+        {
+            // we have the only card that is faceUp?
+            if let indexOfPotentialMatch = indexOfTheOnlyFaceUpCard {
+                // check if the only faceUp card is matching the chosenCard
+                if cards[indexOfPotentialMatch].content == card.content {
+                    // we have a match
+                    cards[chosenIndex].isMatched = true
+                    cards[indexOfPotentialMatch].isMatched = true
+                }
+                // set the index of the only faceUp card to nil because we already have 2 faceUp
+                indexOfTheOnlyFaceUpCard = nil
+            } else {
+                // turn all card to face down
+                for i in cards.indices {
+                    cards[i].isFaceUp = false
+                }
+                // when we turn all cards to facedown we set the chosen card as the onlyFaceUpCard
+                indexOfTheOnlyFaceUpCard = chosenIndex
+            }
+            // then flip it
+            cards[chosenIndex].isFaceUp.toggle()
+        }
+    }
+
+    struct Card: Identifiable {
+        let id: Int
+        var isFaceUp = false
+        var isMatched = false
+        let content: CardContent
+    }
+}

277368342263d2645d5dcd4cabfd6bcb6e196160
yasser tamimi <altamimiy89@gmail.com>
2021-11-25 20:32:41 +0300

    fixed issue with EmojisMemorizeFile

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -8,33 +8,22 @@
 import SwiftUI
 
 class EmojiMemoryGame: ObservableObject {
+    static let emojis = [
+        "🚗", "🚕", "🚙", "🚌", "🚎", "🏎", "🚓", "🚑", "🚒", "🚐", "🛻", "🚚",
+        "🚛", "🚜", "🚍", "🚘", "🚖", "🚇", "🚊", "🛵", "🏍", "🛺", "🚨", "🚔"
+    ]
 
-    static func createMemoryGame(emojis: [String], numberOfPairsOfCards: Int) -> MemoryGame<String> {
-        MemoryGame<String>(numberOfPairsOfCards: numberOfPairsOfCards) { emojis[$0] }
+    static func createMemoryGame(emojis: [String]) -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 8) { emojis[$0] }
     }
 
     // every time in the model we create a new card we'll pass the index so we can get it from our array here
-    @Published private var model: MemoryGame<String>
+    @Published private var model: MemoryGame<String> = createMemoryGame(emojis: emojis)
     var cards: [MemoryGame<String>.Card] { model.cards }
-    private(set) var selectedTheme: Theme
-    var score: Int { model.score }
 
-    init() {
-        self.selectedTheme = Theme.getTheme()
-        let numberOfCards = Int.random(in: 8..<selectedTheme.numbersOfPairOfCards)
-        self.model = EmojiMemoryGame.createMemoryGame(
-            emojis: selectedTheme.emojis,
-            numberOfPairsOfCards: numberOfCards)
-    }
+
 
     // MARK: - Intent(s)
-    func startNewGame() {
-        self.selectedTheme = Theme.getTheme()
-        let numberOfCards = Int.random(in: 8..<selectedTheme.numbersOfPairOfCards)
-        self.model = EmojiMemoryGame.createMemoryGame(
-            emojis: selectedTheme.emojis,
-            numberOfPairsOfCards: numberOfCards)
-    }
     func choose(_ card: MemoryGame<String>.Card) {
         model.choose(card)
     }
